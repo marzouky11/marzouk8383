00512ab26ee7b926a198c8c2abe7a0324e7d48af
Younes Marzouk <ynssmarzouk@gmail.com>
2025-07-25 14:34:51 +0000

    من فضلك أضف كود Google Analytics التالي داخل وسم <head> في الملف src/app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,6 +112,18 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(webSiteSchema) }}
         />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FE0MP7XYXM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FE0MP7XYXM');
+            `,
+          }}
+        />
       </head>
       <body className={cn("font-body antialiased", ptSans.variable)}>
         <ThemeProvider
